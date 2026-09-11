2e0ee41d95fdfcac1a3fa2c3cbed4b2291ab6f9b
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-09-11 22:11:48 +0300

    feat :add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+// import { Routes, Route } from 'react-router-dom'
+import SideBar from './components/SideBar/SideBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-  
+      <SideBar />
+
+      {/* <Routes>
+        <Route path="/" element={<h1>Your Info</h1>} />
+        <Route path="/plan" element={<h1>Select Plan</h1>} />
+        <Route path="/addons" element={<h1>Add-ons</h1>} />
+        <Route path="/summary" element={<h1>Summary</h1>} />
+      </Routes> */}
     </div>
   )
 }
 
 export default App
+
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SideBar() {
   return (
-    <div>
-        
+    <div className="sidebar">
+        <ul className="d-flex flex-column justify-content-center gap-5  align-items-start">
+            <li className="d-flex  align-items-center">
+                <Link to="/">1</Link>
+                
+                <div>
+                <p>Step 1</p>
+                <h2>Your info</h2>
+                </div>
+            </li>
+            <li className="d-flex  align-items-center">
+               <Link to="/plan">2</Link>
+                <div>
+                <p>Step 2</p>
+                <h2>Select plan</h2>
+                </div>
+            </li>
+            <li className="d-flex  align-items-center"   >
+                <Link to="/addons">3</Link>
+                <div>
+                <p>Step 3</p>
+                <h2>Add-ons</h2>
+                </div>
+            </li>
+            <li className="d-flex  align-items-center">
+                <Link to="/summary">4</Link>
+                <div>
+                <p>Step 4</p>
+                <h2>Summary</h2>
+                </div>
+            </li>
+        </ul>
     </div>
   )
 }
