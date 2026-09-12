89cc6031b92a7ad83ff6ec12b89c2bbd15d895ff
Abdulrahim <abdulrhimelsayed@gmail.com>
2026-09-12 14:20:35 +0300

    feat: refactor code of  sideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,39 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function SideBar() {
+    const steps = [
+        { step: 1, label: 'YOUR INFO', path: '/' },
+        { step: 2, label: 'SELECT PLAN', path: '/plan' },
+        { step: 3, label: 'ADD-ONS', path: '/addons' },
+        { step: 4, label: 'SUMMARY', path: '/summary' },
+    ]
   return (
     <div className="sidebar">
-        <ul className="d-flex flex-column justify-content-center gap-5  align-items-start">
-            <li className="d-flex  align-items-center">
-                <Link to="/" className="text-light rounded-circle d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>1</Link>
-                
-                <div className='text-light'>
-                <p>STEP 1</p>
-                <h4 >YOUR INFO</h4>
-                </div>
-            </li>
-            <li className="d-flex  align-items-center">
-               <Link to="/plan" className="text-light rounded-circle d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>2</Link>
-                <div className='text-light'>
-                <p>STEP 2</p>
-                <h4 >SELECT PLAN</h4>
-                </div>
-            </li>
-            <li className="d-flex  align-items-center"   >
-                <Link to="/addons" className="text-light rounded-circle d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>3</Link>
-                <div className='text-light'>
-                <p>STEP 3</p>
-                <h4 >ADD-ONS</h4>
-                </div>
-            </li>
-            <li className="d-flex  align-items-center">
-                <Link to="/summary" className="text-light rounded-circle d-flex align-items-center justify-content-center" style={{ width: '40px', height: '40px' }}>4</Link>
-                <div className='text-light'>
-                <p>STEP 4</p>
-                <h4 >SUMMARY</h4>
-                </div>
-            </li>
+        <ul className="d-flex flex-column justify-content-center list-unstyled gap-5  align-items-start">
+            {
+                steps.map((step)=>{
+                    return (
+                        <li className="d-flex gap-3 align-items-center text-light" key={step.step}>
+                            <NavLink to={step.path} className={
+                                ({isActive}) => isActive ? "d-flex align-items-center justify-content-center border border-light rounded-circle p-2 px-3 text-light text-decoration-none fs-2 bg-primary" :
+                                "d-flex align-items-center justify-content-center border border-light rounded-circle p-2 px-3  text-light text-decoration-none fs-2"
+                            }>
+                                {step.step}
+                            </NavLink>
+                            <div>
+                                <p>STEP {step.step}</p>
+                                <h4>{step.label}</h4>
+                            </div>
+                        </li>
+                    )
+                })
+            }
         </ul>
     </div>
   )
